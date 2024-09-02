ab5d70b24db7ef6e6b8dab6914b4c0e171220d13
MinhCreatorVN <92376650+MinhCreator@users.noreply.github.com>
2024-09-02 20:24:38 +0700

    Update nvim-0_9.lua

--- lua/lazyvim/plugins/compat/nvim-0_9.lua
+++ lua/lazyvim/plugins/compat/nvim-0_9.lua
@@ -1,13 +1,5 @@
 -- Compatibility with Neovim 0.9
 return {
-  -- Use LuaSnip instead of native snippets
-  { "garymjr/nvim-snippets", enabled = false },
-  { import = "lazyvim.plugins.extras.coding.luasnip" },
-
-  -- Use mini.comment instead of ts-comments
-  { "folke/ts-comments.nvim", enabled = false },
-  { import = "lazyvim.plugins.extras.coding.mini-comment" },
-
   -- Use neodev-types with lazydev
   { "folke/neodev.nvim", config = function() end },
   {
